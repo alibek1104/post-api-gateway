3c557c925990c03f15ad28ba7c44546f1321d919
Alibek <nurmahanovalibek@gmail.com>
2023-08-07 23:55:14 +0600

    Initial commit

---
+++ src/main/java/kz/dat/academy/backend/post/api/gateway/PostApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.dat.academy.backend.post.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/dat/academy/backend/post/api/gateway/PostApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dat.academy.backend.post.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
